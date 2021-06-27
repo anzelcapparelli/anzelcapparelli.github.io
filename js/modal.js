9dd76579e7b7d037a31c511a49b5e6015546d4f6
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-06-27 16:59:28 -0700

    got proper modal to open upon clicking the right project; need to fix closing so that clicking outside the modal, or the close button works properly

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,29 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
+var modal = document.getElementsByClassName("projectDesc")[0];
 
 // Get the button that opens the modal
-var toggler = document.getElementById("projectPic");
+// var toggler = document.getElementById("projectPic");
+var toggler = document.getElementsByClassName("projects")[0];
+// var toggler = document.getElementsByClassName("projectPic");
+console.log(toggler.tagName)
+
 
 // Get the <span> element that closes the modal
+
 var span = document.getElementsByClassName("close")[0];
+// console.log(document.getElementsByClassName("close"));
 
 // When the user clicks on the button, open the modal
-toggler.onclick = function() {
-  modal.style.display = "block";
+toggler.onclick = function(event) {
+    if(event.target.tagName !== "IMG") return
+    console.log(event.target.dataset.index)
+    document.getElementById(event.target.dataset.index).style.display = "block";
+//   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function(event) {
   modal.style.display = "none";
 }
 
