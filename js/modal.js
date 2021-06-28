51a6740a349494b2caf566af9e6029ca9aaefb02
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-06-27 17:15:41 -0700

    got modals to close upon clicking outside of them

--- js/modal.js
+++ js/modal.js
@@ -3,10 +3,7 @@
 var modal = document.getElementsByClassName("projectDesc")[0];
 
 // Get the button that opens the modal
-// var toggler = document.getElementById("projectPic");
 var toggler = document.getElementsByClassName("projects")[0];
-// var toggler = document.getElementsByClassName("projectPic");
-console.log(toggler.tagName)
 
 
 // Get the <span> element that closes the modal
@@ -15,21 +12,21 @@ var span = document.getElementsByClassName("close")[0];
 // console.log(document.getElementsByClassName("close"));
 
 // When the user clicks on the button, open the modal
-toggler.onclick = function(event) {
-    if(event.target.tagName !== "IMG") return
-    console.log(event.target.dataset.index)
+toggler.onclick = function (event) {
+    if (event.target.tagName !== "IMG") return
     document.getElementById(event.target.dataset.index).style.display = "block";
-//   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function(event) {
-  modal.style.display = "none";
+span.onclick = function (event) {
+    // parentNode.parentNode
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+
+    if (event.target.classList.contains("modal")) {
+        event.target.style.display = "none";
+    }
 }
