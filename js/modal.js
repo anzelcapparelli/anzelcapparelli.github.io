926313132261639fe2d1c52d6f4e68bd07f65e20
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-06-27 17:27:58 -0700

    got modals to close upon clicking the exit button

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,9 @@
-// Get the modal
-// var modal = document.getElementById("myModal");
-var modal = document.getElementsByClassName("projectDesc")[0];
-
 // Get the button that opens the modal
 var toggler = document.getElementsByClassName("projects")[0];
 
-
 // Get the <span> element that closes the modal
 
-var span = document.getElementsByClassName("close")[0];
+var closer = document.getElementsByClassName("portfolio")[0];
 // console.log(document.getElementsByClassName("close"));
 
 // When the user clicks on the button, open the modal
@@ -18,9 +13,11 @@ toggler.onclick = function (event) {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function (event) {
-    // parentNode.parentNode
-    modal.style.display = "none";
+closer.onclick = function (event) {
+
+    if (event.target.classList.contains("close")) {
+        event.target.parentNode.parentNode.style.display= "none"
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
