5556e21984e23e10ea3a334aabf228bd72205e0b
Anzel Capparelli <anzelcapparelli@gmail.com>
2022-11-27 23:44:54 -0800

    improved project description user experience: improved exit button, improved aesthetics (introduced transparency, improved alignment)

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./style.css";
+import CloseButton from 'react-bootstrap/CloseButton';
 import { v4 as uuidv4 } from 'uuid';
 
 function Project(props) {
@@ -15,9 +16,9 @@ function Project(props) {
     return (
         <div className="card m-5">
             <div
-                className="container"
+                className="container p-0"
             >
-                <h4 className="text-light projectTitle">{props.name}</h4>
+                <h4 className="text-light projectTitle ml-1">{props.name}</h4>
 
                 {/* <div className="container"> */}
                 <img
@@ -25,7 +26,7 @@ function Project(props) {
                     className={`img${props.order} project-img`}
                     alt={props.name}
                     onClick={handleShow}
-                    style={{ opacity: show ? 0.5 : 1 }}
+                    style={{ opacity: show ? 0.1 : 1 }}
                 />
                 {/* change to an 'if' statement, or a ternary operator based on bool setShow! Way to steer away from */}
 
@@ -41,22 +42,25 @@ function Project(props) {
             </div>
 
 
-            <card 
-            className="text-light projectDescription" 
-            show={show} 
-            style={{ opacity: show ? 1 : 0 }}
-            onClick={show? "":{handleShow}}
+            <card
+                className="text-light projectDescription"
+                show={show}
+                style={{ opacity: show ? 1 : 0 }}
+                onClick={show ? "" : { handleShow }}
             // ^^^ onClick not currently working as intended
             >
-                <body className="text-light">
-                    <span
-                        className="descriptionClose"
+                <hr className="descriptionTop"></hr>
+                <body className="text-light descriptionBG">
+                    <CloseButton
+                        className="descriptionClose mb-2 mr-2"
+                        variant="white"
                         onClick={handleClose}
                         style={show ? { cursor: "pointer" } : {}}
+                    />
+                    <br />
+                    <ul
+                    className="descriptionList"
                     >
-                        x
-                    </span>
-                    <ul>
                         {props.desc.map(tidbit =>
                             <li key={uuidv4()}>{tidbit}</li>
                         )}
