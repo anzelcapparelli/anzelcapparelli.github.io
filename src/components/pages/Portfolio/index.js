e65785498501cc42372b189a6109c7e5863274e1
Anzel Capparelli <anzelcapparelli@gmail.com>
2023-08-07 20:43:54 -0700

    removed nonfunctional projects, due to old hosting website no longer providing free service. Need to reformat projects so that they show again

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Project from "../../Project";
 import "../../transparent.css";
+import DescriptionToast from "../../DescriptionToast";
 
 function Portfolio() {
 
@@ -18,45 +19,46 @@ function Portfolio() {
       ],
       githubHref: "https://github.com/anzelcapparelli/marvelHeroFinder/"
     },
+    
+        {
+          order:2,
+          name: "Google Books",
+          imgSrc: process.env.PUBLIC_URL + "/assets/images/googleBooks.jpg",
+          deployedHref: "https://google-books379009.herokuapp.com/",
+        desc: [
+        "Full-Stack MERN application that stores favorite book searches, and allows removal from saved list",
+        "Used Google Books API for data and utilized prop-drilling in the React Components",
+        "Technologies: HTML5, CSS, JavaScript, MERN, Mongoose, React-Bootstrap",
+        ],
+          githubHref: "https://github.com/anzelcapparelli/google_books_search"
+        },
 
-    {
-      order:2,
-      name: "The Brew Budz",
-      imgSrc: process.env.PUBLIC_URL + "/assets/images/bb-landing.jpg",
-      deployedHref: "https://thebrewbudz.herokuapp.com/landing",
-      desc: [
-        "Full-Stack application that included database storage of information, and user-saved results",
-        "Created database models, and most JavaScript-database API coding. Was also a go-to for coding issues.",
-        "Technologies: JavaScript, Express Handlebars, Passport.js, Sequelize, MySQL"
-      ],
-      githubHref: "https://github.com/Guled06/the-brew-budz"
-    },
+    // {
+    //   order:3,
+    //   name: "The Brew Budz",
+    //   imgSrc: process.env.PUBLIC_URL + "/assets/images/bb-landing.jpg",
+    //   deployedHref: "https://thebrewbudz.herokuapp.com/landing",
+    //   desc: [
+    //     "Full-Stack application that included database storage of information, and user-saved results",
+    //     "Created database models, and most JavaScript-database API coding. Was also a go-to for coding issues.",
+    //     "Technologies: JavaScript, Express Handlebars, Passport.js, Sequelize, MySQL"
+    //   ],
+    //   githubHref: "https://github.com/Guled06/the-brew-budz"
+    // },
 
-    {
-      order:3,
-      name: "Eat-Da-Burger!",
-      imgSrc: process.env.PUBLIC_URL + "/assets/images/burgerLaunch.jpg",
-      deployedHref: "https://immense-everglades-88150.herokuapp.com/",
-      desc: [
-        "Full-stack app that allows addition of new burgers to eat, and tracks those eaten",
-        "Used MySQL to store both burgers to eat and burgers eaten, and used ORM to compartmentalize MySQL queries into one file",
-        "Technologies: HTML5, CSS, JavaScript, MySQL, ORM, Express Handlebars",
-      ],
-      githubHref: "https://github.com/anzelcapparelli/burger"
-    },
+    // {
+    //   order:4,
+    //   name: "Eat-Da-Burger!",
+    //   imgSrc: process.env.PUBLIC_URL + "/assets/images/burgerLaunch.jpg",
+    //   deployedHref: "https://immense-everglades-88150.herokuapp.com/",
+    //   desc: [
+    //     "Full-stack app that allows addition of new burgers to eat, and tracks those eaten",
+    //     "Used MySQL to store both burgers to eat and burgers eaten, and used ORM to compartmentalize MySQL queries into one file",
+    //     "Technologies: HTML5, CSS, JavaScript, MySQL, ORM, Express Handlebars",
+    //   ],
+    //   githubHref: "https://github.com/anzelcapparelli/burger"
+    // },
 
-    {
-      order:4,
-      name: "Google Books",
-      imgSrc: process.env.PUBLIC_URL + "/assets/images/googleBooks.jpg",
-      deployedHref: "https://google-books379009.herokuapp.com/",
-    desc: [
-    "Full-Stack MERN application that stores favorite book searches, and allows removal from saved list",
-    "Used Google Books API for data and utilized prop-drilling in the React Components",
-    "Technologies: HTML5, CSS, JavaScript, MERN, Mongoose, React-Bootstrap",
-    ],
-      githubHref: "https://github.com/anzelcapparelli/google_books_search"
-    },
 
     // {
       // order:5,
@@ -83,11 +85,12 @@ function Portfolio() {
     // ],
     //   githubHref:
     // },
+    /* <DescriptionToast>
+    </DescriptionToast> */
   ]
 
   return (
     <div>
-
       {projectsArr.map(projArrRow =>
         <Project {...projArrRow} key={projArrRow.name} />
       )}
